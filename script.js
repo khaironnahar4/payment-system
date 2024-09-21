a01110b638b50c74d86c99cd03b1ce27b1c1400d
Khairon Nahar <khaironnahar247@gmail.com>
2024-09-21 12:02:16 +0600

    I created the cash out and money transfer funtionality

--- script.js
+++ script.js
@@ -19,3 +19,40 @@ document.getElementById("add-money-submit-button")
     
 })
 
+// cash out functionality
+document.getElementById("cash-out-submit-button")
+.addEventListener("click", function(e){
+    e.preventDefault();
+    const amount = inputFieldValueById("cash-out-amount");
+    const pin = inputFieldValueById("cash-out-pin");
+    if(pin === 1234){
+        const totalAmount = textFieldValueById("total-balance") ;
+        if(totalAmount>=amount){
+            document.getElementById("total-balance").innerText = totalAmount - amount;
+        }else{
+            alert("You don't have sufficient amount!");
+        }
+    }else{
+        alert("Wrond pin number")
+    }
+})
+
+// money transfer functionality
+document.getElementById("money-transfer-submit-button")
+.addEventListener("click", function(e){
+    e.preventDefault();
+    const amount = inputFieldValueById("money-transfer-amount");
+    const pin = inputFieldValueById("money-transfer-pin");
+    if(pin === 1234){
+        const totalAmount = textFieldValueById("total-balance") ;
+        if(totalAmount>=amount){
+            document.getElementById("total-balance").innerText = totalAmount - amount;
+            const number =  inputFieldValueById("money-transfer-number");
+            document.getElementById("money-transfer-confirmation").innerText = `${amount}Tk has been transfered to ${number}. `
+        }else{
+            alert("You don't have sufficient amount!");
+        }
+    }else{
+        alert("Wrond pin number")
+    }
+})
